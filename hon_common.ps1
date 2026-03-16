5c36fc4d22e8e683c7d7dd3c38d4195c406c302c
Xyling12 <xyling123@gmail.com>
2026-03-16 22:41:29 +0400

    fix: launcher freeze on updating by removing dangerous React property string translations

--- hon_common.ps1
+++ hon_common.ps1
@@ -144,10 +144,6 @@ function Get-DirectDropboxUrl([string]$url) {
 }
 function Get-HoNWebOverrideJsReplacements {
     return @(
-        [pscustomobject]@{ Source = '"Username"'; Target = '"\u0418\u043c\u044f \u043f\u043e\u043b\u044c\u0437\u043e\u0432\u0430\u0442\u0435\u043b\u044f"' }
-        [pscustomobject]@{ Source = '"Password"'; Target = '"\u041f\u0430\u0440\u043e\u043b\u044c"' }
-        [pscustomobject]@{ Source = '"Remember Me"'; Target = '"\u0417\u0430\u043f\u043e\u043c\u043d\u0438\u0442\u044c \u043c\u0435\u043d\u044f"' }
-        [pscustomobject]@{ Source = '"Login"'; Target = '"\u0412\u043e\u0439\u0442\u0438"' }
         [pscustomobject]@{ Source = '"Message of the Day"'; Target = '"\u0421\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u0435 \u0434\u043d\u044f"' }
         [pscustomobject]@{ Source = '"Latest Updates"'; Target = '"\u041f\u043e\u0441\u043b\u0435\u0434\u043d\u0438\u0435 \u043e\u0431\u043d\u043e\u0432\u043b\u0435\u043d\u0438\u044f"' }
         [pscustomobject]@{ Source = '"Quick Links"'; Target = '"\u0411\u044b\u0441\u0442\u0440\u044b\u0435 \u0441\u0441\u044b\u043b\u043a\u0438"' }
